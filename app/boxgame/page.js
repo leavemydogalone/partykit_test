19c8b963357a88d7c315406bd7d946562ce8d04c
leavemydogalone <niklasport88@gmail.com>
2023-10-20 18:02:21 -0700

    got the dragger moving on frontend

--- app/boxgame/page.js
+++ app/boxgame/page.js
@@ -25,29 +25,18 @@ export default function BoxContainer() {
     },
   });
 
-  function handleMove(id, position) {
-    // ws.send(JSON.stringify({ id: id, position: position }));
-    setBoxes((prev) =>
-      prev.map((box) => {
-        if (box.id !== id) {
-          return { ...box };
-        } else {
-          console.log(boxes);
-          return { ...box, x: position.x, y: position.y };
-        }
-      })
-    );
-  }
-
   return (
-    <main id="main" className={styles.boxContainer}>
-      {boxes.map((box) => (
-        <Box
-          key={box.id}
-          props={{ x: box.x, y: box.y, id: box.id }}
-          handleMove={handleMove}
-        />
-      ))}
+    <main id="main" className={styles.main}>
+      <section className={styles.boxContainer}>
+        {boxes.map((box) => (
+          <Box
+            key={box.id}
+            props={{ x: box.x, y: box.y, id: box.id }}
+            // handleMove={handleMove}
+            setBoxes={setBoxes}
+          />
+        ))}
+      </section>
     </main>
   );
 }
--- app/components/Box.js
+++ app/components/Box.js
@@ -1,39 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./Box.module.css";
 
-export default function Box({ props, handleMove }) {
-  const [selected, setSelected] = useState(false);
+export default function Box({ props, setBoxes }) {
+  const [offsetX, setOffsetX] = useState(0);
+  //   the left offset from parent div of the draggerContainer
+  const [isDown, setIsDown] = useState(false);
+  //   if the mouse is held down
+  const [draggerPositionX, setDraggerPositionX] = useState(0);
+  //   The "left" postition of the draggerContainer
+
+  const box = useRef(null);
 
   const { x, y, id } = props;
   let transform = { transform: `translate(${x}px, ${y}px)` };
 
-  function handleMouseDown(event) {
-    setSelected(true);
-    console.log(`selected = ${selected}`);
+  function handleMove(id, position) {
+    // ws.send(JSON.stringify({ id: id, position: position }));
+    setBoxes((prev) =>
+      prev.map((box) => {
+        if (box.id !== id) {
+          return { ...box };
+        } else {
+          // console.log(boxes);
+          return { ...box, x: position.x, y: 0 };
+        }
+      })
+    );
   }
+
   function handleMouseMove(event) {
     if (!selected) {
       return;
     } else {
-      let bodyRect = document.body.getBoundingClientRect(),
+      let bodyRect = box.current.getBoundingClientRect(),
         elemRect = event.target.getBoundingClientRect(),
-        offset = elemRect.top - bodyRect.top;
-      // console.log(elemRect);
-      // console.log(event.clientX);
+        offsetX = bodyRect.left - elemRect.left,
+        offsetY = elemRect.top - bodyRect.top;
 
-      handleMove(id, { x: event.clientX, y: event.clientY });
+      handleMove(id, {
+        x: offsetX + event.clientX,
+        y: offsetY + event.clientY,
+      });
     }
   }
+
   return (
     <div
+      ref={box}
       className={styles.box}
-      style={transform}
-      onMouseDown={(e) => handleMouseDown(e)}
-      onMouseLeave={() => setSelected(false)}
-      onMouseUp={() => setSelected(false)}
-      onMouseMove={(e) => handleMouseMove(e)}
+      style={{ left: draggerPositionX }}
+      onMouseUp={() => setIsDown(false)}
+      onMouseLeave={() => setIsDown(false)}
+      onMouseMoveCapture={(e) => {
+        if (isDown) {
+          setDraggerPositionX(offsetX + e.clientX);
+          // if the mouse is down, will set the "left" position of the dragger,
+          //   based on the offsetLeft of the draggerContainer when the mouse went down
+          // plus the mouse position
+          if (offsetX + e.clientX < 0) {
+            setDraggerPositionX(0);
+          }
+          //   prevents the dragger from being dragged out of the left of the container
+        }
+      }}
+      onMouseDown={(e) => {
+        setIsDown(true);
+        const nullChecker =
+          box.current !== null &&
+          setOffsetX(box.current.offsetLeft - e.clientX);
+      }}
     >
-      Im a box
+      <span>Box</span>
     </div>
   );
 }
